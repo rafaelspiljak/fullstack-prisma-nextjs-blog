92d1d352d1308b012e3b1b98b320023d1cd0f78b
rafaelspiljak <44222850+rafaelspiljak@users.noreply.github.com>
2023-10-24 09:05:51 +0200

    feat: icons and loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -134,99 +134,134 @@ const Blog: React.FC<Props> = (props) => {
   );
   const selectedDay = "border-b-2 border-b-green-600";
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   return (
     <Layout>
-      <div className="page">
-        <main>
-          <div className="bg-gray-400 p-2 m-2 rounded-lg grid grid-cols-2 xs:grid-cols-3 md:grid-cols-4 lg:grid-cols-8">
-            {Object.keys(props.timeSlotsByDay).map((dayIndex, index) => {
-              return (
-                <div
-                  key={dayIndex}
-                  onClick={() => {
-                    setSelectedDateIndex(dayIndex);
-                  }}
-                  className={`${
-                    (selectedDateIndex === null && index === 0) ||
-                    selectedDateIndex === dayIndex
-                      ? selectedDay
-                      : ""
-                  } cursor-pointer `}
-                >
-                  {dayIndex}
-                </div>
-              );
-            })}
-          </div>
-          <div className="bg-gray-500 p-2 rounded-lg m-2">
-            {Object.keys(props.timeSlotsByDay).map((dayIndex, index) => {
-              if (
-                (index === 0 && selectedDateIndex === null) ||
-                selectedDateIndex === dayIndex
-              ) {
-                return props.timeSlotsByDay[dayIndex].map((post) => (
-                  <div key={post.id} className="gap-2 flex items-center my-2">
-                    <span>{post.id.split(":00.000")[0].replace("T", " ")}</span>
-                    {!!session ? (
-                      <>
-                        {!post.reservedBy ? (
-                          <button
-                            className="m-2 p-2 bg-gray-600 rounded-lg text-white"
-                            onClick={async () => {
-                              try {
-                                await fetch(`/api/reserve-slot/`, {
-                                  method: "POST",
-                                  body: JSON.stringify({ id: post.id }),
-                                });
-                                toast.success("Rezervacija uspješna");
-                                setTimeout(() => {
-                                  router.reload();
-                                }, 1000);
-                              } catch (e) {
-                                toast.error("Pokušajte ponovno");
-                              }
-                            }}
-                          >
-                            Rezerviraj termin
-                          </button>
-                        ) : post.reservedBy?.id == session?.user?.id ? (
-                          <button
-                            className="m-2 p-2 bg-red-400 rounded-lg text-white"
-                            onClick={async () => {
-                              try {
-                                await fetch(`/api/reserve-slot/${post.id}`, {
-                                  method: "DELETE",
-                                });
-                                toast.success(
-                                  "Otkazivanje rezervacije uspješno"
-                                );
-
-                                setTimeout(() => {
-                                  router.reload();
-                                }, 1000);
-                              } catch (e) {
-                                toast.error("Pokušajte ponovno");
-                              }
-                            }}
-                          >
-                            Otkaži rezervaciju
-                          </button>
-                        ) : null}
-                      </>
-                    ) : !!post.reservedBy ? (
-                      <span>Rezervirano</span>
-                    ) : (
-                      <span>Slobodan termin</span>
-                    )}
+      {isLoading ? (
+        <div className="h-[100vh]">
+          <svg
+            aria-hidden="true"
+            className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-red-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      ) : (
+        <div className="page">
+          <main>
+            <div className="bg-gray-400 p-2 m-2 rounded-lg grid grid-cols-2 xs:grid-cols-3 md:grid-cols-4 lg:grid-cols-8">
+              {Object.keys(props.timeSlotsByDay).map((dayIndex, index) => {
+                return (
+                  <div
+                    key={dayIndex}
+                    onClick={() => {
+                      setSelectedDateIndex(dayIndex);
+                    }}
+                    className={`${
+                      (selectedDateIndex === null && index === 0) ||
+                      selectedDateIndex === dayIndex
+                        ? selectedDay
+                        : ""
+                    } cursor-pointer `}
+                  >
+                    {dayIndex}
                   </div>
-                ));
-              }
+                );
+              })}
+            </div>
+            <div className="bg-gray-500 p-2 rounded-lg m-2">
+              {Object.keys(props.timeSlotsByDay).map((dayIndex, index) => {
+                if (
+                  (index === 0 && selectedDateIndex === null) ||
+                  selectedDateIndex === dayIndex
+                ) {
+                  return props.timeSlotsByDay[dayIndex].map((post) => (
+                    <div key={post.id} className="gap-2 flex items-center my-2">
+                      <span>
+                        {post.id.split(":00.000")[0].replace("T", " ")}
+                      </span>
+                      {!!session ? (
+                        <>
+                          {!post.reservedBy ? (
+                            <button
+                              className="m-2 p-2 bg-gray-600 rounded-lg text-white"
+                              onClick={async () => {
+                                setIsLoading(true);
+                                try {
+                                  await fetch(`/api/reserve-slot/`, {
+                                    method: "POST",
+                                    body: JSON.stringify({ id: post.id }),
+                                  });
+                                  toast.success("Rezervacija uspješna", {
+                                    icon: "🎾",
+                                  });
+                                  setTimeout(() => {
+                                    router.reload();
+                                  }, 2000);
+                                } catch (e) {
+                                  toast.error("Pokušajte ponovno");
+                                } finally {
+                                  setIsLoading(false);
+                                }
+                              }}
+                            >
+                              Rezerviraj termin
+                            </button>
+                          ) : post.reservedBy?.id == session?.user?.id ? (
+                            <button
+                              className="m-2 p-2 bg-red-400 rounded-lg text-white"
+                              onClick={async () => {
+                                setIsLoading(true);
 
-              return null;
-            })}
-          </div>
-        </main>
-      </div>
+                                try {
+                                  await fetch(`/api/reserve-slot/${post.id}`, {
+                                    method: "DELETE",
+                                  });
+                                  toast.success(
+                                    "Otkazivanje rezervacije uspješno",
+                                    { icon: "🎾" }
+                                  );
+
+                                  setTimeout(() => {
+                                    router.reload();
+                                  }, 2000);
+                                } catch (e) {
+                                  toast.error("Pokušajte ponovno");
+                                } finally {
+                                  setIsLoading(false);
+                                }
+                              }}
+                            >
+                              Otkaži rezervaciju
+                            </button>
+                          ) : null}
+                        </>
+                      ) : !!post.reservedBy ? (
+                        <span>Rezervirano</span>
+                      ) : (
+                        <span>Slobodan termin</span>
+                      )}
+                    </div>
+                  ));
+                }
+
+                return null;
+              })}
+            </div>
+          </main>
+        </div>
+      )}
     </Layout>
   );
 };
